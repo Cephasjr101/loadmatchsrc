3f7ec6236b0e8ed60a08153ce5c1afc5c017191c
Cephasjr101 <dominioncovenantcompany@gmail.com>
2026-09-18 10:16:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import logging
 import os
-from app.routers import auth_google
-app.include_router(auth_google.router)
 from pathlib import Path
 
 from fastapi import Depends, FastAPI, HTTPException
